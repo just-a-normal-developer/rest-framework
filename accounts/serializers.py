3bc8951ca545c15c960d3a69d98809e070af0d99
mhmd <sherkatfiles0214@yahoo.com>
2023-06-14 16:42:29 +0330

    the bug fixed the problem was related to the s aftre validator it was validators(dictation was wrong)

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
 
 
 def clean_email(value):
@@ -7,7 +7,23 @@ def clean_email(value):
         raise serializers.ValidationError('admin email is not valid !! :)')
 
 
-class UserRegisterSerializer(serializers.Serializer):
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only = True , required = True)
+
+    class Meta:
+        model = User
+        fields =(
+            'username',
+            'email',
+            'password',
+            'password2',
+
+        )
+        extra_kwargs = {
+            'password' : {'write_only' : True},
+            'email' : {'validators' : (clean_email,)}
+
+        }
     # username = serializers.CharField(required = True)
     # email = serializers.EmailField(required = True , validators = [clean_email])
     # password = serializers.CharField(required = True , write_only = True)
