f9009c1eb3ef243111ceb0214a3e19e7b5ec3328
mhmd <sherkatfiles0214@yahoo.com>
2023-07-17 16:02:38 +0330

    some changes added in admin part beacause the clean code say it is better to have meaningfull coments

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
+#actually for having the access in model attributes you need to use decorator or if you are using the class based code you should use inheritance to register the admin on the model
