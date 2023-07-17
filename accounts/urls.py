51da3c4ddcf6ae4210e6a963a75366badef7ae67
mhmd <sherkatfiles0214@yahoo.com>
2023-07-17 16:08:10 +0330

    It understand the concept better now

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from . import views
 from rest_framework.authtoken import views as auth_token
 
 #here the app name added beacause when you want to use the  name space in the core url then it is needed to have  the app name in the url part of your code
+#actually it is important here
+
 
 app_name = 'accounts'
 
