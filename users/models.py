929314c51dcd1454a420e9b0ac776350d55436ed
mhmd <sherkatfiles0214@yahoo.com>
2023-07-22 16:13:45 +0330

    the name of the answer changed in the model

--- users/models.py
+++ users/models.py
@@ -30,6 +30,8 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
+    class Meta:
+        verbose_name_plural = 'answer'
     user = models.ForeignKey(User , on_delete=models.CASCADE , related_name='answers')
     question = models.ForeignKey(Question , on_delete=models.CASCADE , related_name='qanswers')
     body = models.TextField()
