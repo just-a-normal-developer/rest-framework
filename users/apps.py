f32ad24b0ba483ecd138dfb131196fc2b7162690
mhmd.s <132132219+just-a-normal-developer@users.noreply.github.com>
2023-07-30 17:34:28 +0330

    Update apps.py

--- users/apps.py
+++ users/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+#note that for the signals we need to define here
